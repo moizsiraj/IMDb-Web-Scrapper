14b84afac5249b6884befe26cb6eb24d5f5ca36b
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2020-12-16 21:15:52 +0500

    generate top 50 movies'data for 2016-2020

--- imdbMovies.py
+++ imdbMovies.py
@@ -5,96 +5,98 @@ import re
 driver = webdriver.Chrome(
     'D:/Google Drive/University/7 Sem/DW/Assignments/web scrapping/chromedriver_win32/chromedriver')
 
-myUrl = "https://www.imdb.com/search/title/?title_type=feature&release_date=2015-01-01,2015-12-31&adult=include&sort=boxoffice_gross_us,desc"
+file = open('yearly movies.txt', 'r')
+Lines = file.readlines()
+for line in Lines:
+    # myUrl = "https://www.imdb.com/search/title/?title_type=feature&release_date=2015-01-01,2015-12-31&adult=include&sort=boxoffice_gross_us,desc"
+    myUrl = line
+    pageHTML = driver.get(myUrl)
+    content = driver.page_source
+    pageSoup = Soup(content, "html.parser")
 
-pageHTML = driver.get(myUrl)
-content = driver.page_source
-pageSoup = Soup(content, "html.parser")
+    # getting movie titles
+    movieItem = pageSoup.findAll("div", {"class": "lister-item-content"})
+    for i in range(0, 50):
+        movieUrl = 'https://www.imdb.com/' + movieItem[i].h3.a['href']
+        subPageHTML = driver.get(movieUrl)
 
-# getting movie titles
-movieItem = pageSoup.findAll("div", {"class": "lister-item-content"})
+        # navigating to the movie pgae
+        content = driver.page_source
+        subPageSoup = Soup(content, "html.parser")
 
-movieUrl = 'https://www.imdb.com/' + movieItem[0].h3.a['href']
+        # getting title
+        if subPageSoup.find('div', attrs={'class': 'title_wrapper'}) is not None:
+            movie = subPageSoup.find('div', attrs={'class': 'title_wrapper'})
+            title = movie.h1.text.strip()
+            print(title)
 
-subPageHTML = driver.get(movieUrl)
+        # get rating
+        if subPageSoup.find('div', attrs={'class': 'ratingValue'}) is not None:
+            ratingHTML = subPageSoup.find('div', attrs={'class': 'ratingValue'})
+            rating = ratingHTML.span.text
+            print(rating)
 
-# navigating to the movie pgae
-content = driver.page_source
-subPageSoup = Soup(content, "html.parser")
+        # get storyline
+        if subPageSoup.find('div', attrs={'class': 'summary_text'}) is not None:
+            storyHTML = subPageSoup.find('div', attrs={'class': 'summary_text'})
+            print(storyHTML.text.strip())
 
-# getting title
-if subPageSoup.find('div', attrs={'class': 'title_wrapper'}) is not None:
-    movie = subPageSoup.find('div', attrs={'class': 'title_wrapper'})
-    title = movie.h1.text.strip()
-    print(title)
+        # get genre
+        for item in subPageSoup.findAll('div', attrs={'class': 'see-more inline canwrap'}):
+            if item.h4 == None:
+                break
+            if item.h4.text == 'Genres:' or item.h4.text == 'Genre:':
+                genre = ''
+                for val in item.findAll('a'):
+                    genre = genre + val.text + ','
+                print(genre)
 
-# get rating
-if subPageSoup.find('div', attrs={'class': 'ratingValue'}) is not None:
-    ratingHTML = subPageSoup.find('div', attrs={'class': 'ratingValue'})
-    rating = ratingHTML.span.text
-    print(rating)
+        # get stars
+        for item in subPageSoup.findAll('div', attrs={'class': 'credit_summary_item'}):
+            if item.h4 == None:
+                break
+            if item.h4.text == 'Stars:' or item.h4.text == 'Star:':
+                for val in item.findAll('a'):
+                    if val.text == 'See full cast & crew':
+                        break;
+                    print(val.text)
 
-# get storyline
-if subPageSoup.find('div', attrs={'class': 'summary_text'}) is not None:
-    storyHTML = subPageSoup.find('div', attrs={'class': 'summary_text'})
-    print(storyHTML.text.strip())
+        # get direction
+        for item in subPageSoup.findAll('div', attrs={'class': 'credit_summary_item'}):
+            if item.h4 == None:
+                break
+            if item.h4.text == 'Directors:' or item.h4.text == 'Director:':
+                for val in item.findAll('a'):
+                    print(val.text)
 
-# get genre
-for item in subPageSoup.findAll('div', attrs={'class': 'see-more inline canwrap'}):
-    if item.h4 == None:
-        break
-    if item.h4.text == 'Genres:' or item.h4.text == 'Genre:':
-        genre = ''
-        for val in item.findAll('a'):
-            genre = genre + val.text + ','
-        print(genre)
+        # get budget
+        for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
+            if item.h4 == None:
+                break
+            if item.h4.text == 'Budget:':
+                text = item.text.strip()
+                result = re.search('Budget:(.*)', text)
+                amount = result.group(1)
+                amount = re.sub('[!@#$.,]', '', amount)
+                print(amount)
 
-# get stars
-for item in subPageSoup.findAll('div', attrs={'class': 'credit_summary_item'}):
-    if item.h4 == None:
-        break
-    if item.h4.text == 'Stars:' or item.h4.text == 'Star:':
-        for val in item.findAll('a'):
-            if val.text == 'See full cast & crew':
+        # get gross
+        for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
+            if item.h4 == None:
+                break
+            if item.h4.text == 'Cumulative Worldwide Gross:':
+                text = item.text.strip()
+                result = re.search('Cumulative Worldwide Gross:(.*)', text)
+                amount = result.group(1)
+                amount = re.sub('[!@#$.,]', '', amount)
+                print(amount)
+
+        # get tagline
+        for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
+            if item.h4 == None:
                 break;
-            print(val.text)
-
-# get direction
-for item in subPageSoup.findAll('div', attrs={'class': 'credit_summary_item'}):
-    if item.h4 == None:
-        break
-    if item.h4.text == 'Directors:' or item.h4.text == 'Director:':
-        for val in item.findAll('a'):
-            print(val.text)
-
-# get budget
-for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
-    if item.h4 == None:
-        break
-    if item.h4.text == 'Budget:':
-        text = item.text.strip()
-        result = re.search('Budget:(.*)', text)
-        amount = result.group(1)
-        amount = re.sub('[!@#$.,]', '', amount)
-        print(amount)
-
-# get gross
-for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
-    if item.h4 == None:
-        break
-    if item.h4.text == 'Cumulative Worldwide Gross:':
-        text = item.text.strip()
-        result = re.search('Cumulative Worldwide Gross:(.*)', text)
-        amount = result.group(1)
-        amount = re.sub('[!@#$.,]', '', amount)
-        print(amount)
-
-# get tagline
-for item in subPageSoup.findAll('div', attrs={'class': 'txt-block'}):
-    if item.h4 == None:
-        break;
-    if item.h4.text == 'Tagline:' or item.h4.text == 'Taglines:':
-        text = item.text.strip()
-        result = re.search('Taglines:\n(.*)', text)
-        tagline = result.group(1)
-        print(tagline)
+            if item.h4.text == 'Tagline:' or item.h4.text == 'Taglines:':
+                text = item.text.strip()
+                result = re.search('Taglines:\n(.*)', text)
+                tagline = result.group(1)
+                print(tagline)
